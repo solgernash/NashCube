055a2a1e6d4ea87d57d6d4aa6d26822663fe46c6
solgernash <ethanapplecarroll@gmail.com>
2026-07-23 17:55:45 -0400

    Got a single frame output of the color red! Learned about proper c++ syntax and libogc calls

--- source/main.cpp
+++ source/main.cpp
@@ -1,1 +1,23 @@
-int main(void) {return 0;} //stub to get makefile working
+#include <gccore.h>
+#include <malloc.h>
+
+
+int main(void) {
+
+    VIDEO_Init();
+    GXRModeObj *rmode = VIDEO_GetPreferredMode(NULL);
+    void *fb = memalign(0, VIDEO_GetFrameBufferSize(rmode));
+
+
+    VIDEO_Configure(rmode);
+    VIDEO_ClearFrameBuffer(rmode, fb, COLOR_RED);
+    VIDEO_SetNextFramebuffer(fb);
+    VIDEO_SetBlack(FALSE);
+    VIDEO_Flush();
+    VIDEO_WaitVSync();
+
+    while (1) {
+        VIDEO_WaitVSync();
+    } //while (1)
+
+} //main
